1c84e9b14861b0c99ebfcdcd4506beb903eec766
Praveen BM <praveen.mahabal@hotmail.com>
2017-04-21 21:18:56 +0530

    icon change

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,16 +1,19 @@
 import React, {PropTypes} from 'react';
 import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps';
+import  logoimg from '../../images/car-loc.png';
 
 const map = ({center,onMapMounted,onDragend,markers,onSlotSelected}) =>{
         const mapContainer = <div style={{height:'100%', width:'100%'}}></div>;
+        debugger;
         const marker = markers.map((venue,i) => {
             const marker = {
                 position:{
                     lat:parseFloat(venue.geoLocation.coordinates[1]),
                     lng:parseFloat(venue.geoLocation.coordinates[0])
                 }
+    
             };
-            return <Marker key={i} {...marker} onClick={()=>onSlotSelected(venue)} />;
+            return <Marker key={i} {...marker} onClick={()=>onSlotSelected(venue)} icon={logoimg} defaultAnimation='2' />;
         });
      return (
         <GoogleMapLoader   containerElement = {mapContainer} googleMapElement = {
@@ -30,3 +33,5 @@ map.propTypes = {
 };
 
 export default map;
+
+
